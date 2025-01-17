2ee6fd3407594c9437ed797eabb2021808cb51a7
NexusFinanceAi <ipufezabo356@gmail.com>
2025-01-17 19:31:58 +0900

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,28 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    base: {
+      url: process.env.BASE_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: {
+      base: process.env.BASESCAN_API_KEY,
+    },
+    customChains: [
+      {
+        network: "base",
+        chainId: 8453,
+        urls: {
+         apiURL: "https://api.basescan.org/api",
+         browserURL: "https://basescan.org"
+        }
+      }
+    ]
+  },
+};
